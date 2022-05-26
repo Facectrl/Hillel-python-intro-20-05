15fedebcb3ed199bcbc1b3602e00c3f4c3769b15
Alexandr Navrotskyi <ledeventsmasters@gmail.com>
2022-05-27 02:51:05 +0300

    # this commit,my first commit

--- Operators/Operators.py
+++ Operators/Operators.py
@@ -1,4 +1,6 @@
-a = int(input())
-b = a
-c = b + a
-print(a, c, b)
+group1 = int(input())  # детей в 1 группе
+group2 = int(input())  # детей в 2 группе
+sum_group = group1 + group2  # общее колличество,детей
+remain = sum_group % 2  # Остаток от деления
+bed = sum_group // 2 + remain  # В 1 кровати до 2-х детей + остаток
+print(bed)
