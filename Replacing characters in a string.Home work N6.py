80b597df2480969f721c7c54f728bcddbd2d7b49
Alexandr Navrotskyi <ledeventsmasters@gmail.com>
2022-06-04 03:47:51 +0300

    # (Random, list)++

--- Replacing characters in a string.Home work N6.py
+++ Replacing characters in a string.Home work N6.py
@@ -1,12 +1,12 @@
 text: str = input("")
 text[:text.find(" ")]
 text[text.rfind(" "):]
-text[text.find(' '):text.rfind(' ')]
-print(text[:text.find(" ")], text[text.find(' '):text.rfind(' ')].replace('n', 'N'), text[text.rfind(" "):], sep='')
+text[text.find(" "):text.rfind(" ")]
+print(text[:text.find(" ")], text[text.find(" "):text.rfind(" ")].replace('n', 'N'), text[text.rfind(" "):], sep='')
 #
 #читаемый > эффективный?
 text: str = input("")
 a = text[:text.find(" ")]
 b = text[text.rfind(" "):].replace('n', 'N')
-c = text[text.find(' '):text.rfind(' ')]
+c = text[text.find(" "):text.rfind(" ")]
 print(a, b, c, sep='')
