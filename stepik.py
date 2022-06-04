a27cc4bc9fa5282292966fc9807923ee46b95e10
Facectrl <106082075+Facectrl@users.noreply.github.com>
2022-06-04 13:51:51 +0300

    Delete stepik.py

--- stepik.py
+++
@@ -1,5 +0,0 @@
-text: str = input("")
-text[:text.find(" ")]
-text[text.rfind(" "):]
-text[text.find(" "):text.rfind(" ")]
-print(text[:text.find(" ")], text[text.find(" "):text.rfind(" ")].replace('n', 'N'), text[text.rfind(" "):], sep='')
