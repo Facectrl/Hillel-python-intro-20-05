5b72550d13a812f2180698a6c2e19e6d55eb4ea9
Trueman <ledeventsmasters@gmail.com>
2022-06-12 00:59:34 +0300

    Не использовать только (if)

--- Home work N9.Neighbor  numbers.py
+++ Home work N9.Neighbor  numbers.py
@@ -1,4 +1,3 @@
-import random
 # Сгенерировать список случайных целых чисел от 0 до N (N вводит пользователь).
 # После этого он еще может ввести число,
 # которое программа должна постараться найти в списке и вывести на экран его соседей (числа слева и справа).
@@ -8,19 +7,20 @@ import random
 # Индексы в цикле можно получить функцией enumerate.
 
 
+import random
+
 N = int(input('Enter number: '))
 list1 = [random.randint(0, N) for i in range(0, N)]  # Сгенерировать список случайных целых чисел от 0 до N
 print('Random number list is: ', list1)
 X = int(input('Enter number to find: '))
 for i, l1 in enumerate(list1):  # Индексы в списке
     if l1 == X:  # Проверка есть ли число в списке
-        if i == 0:
+        if i == 0:  # if begin
             print('Number begin in list:', list1[i + 1])
-            if i == len(list1) - 1:
-                print('Number end in list:', list1[i - 1])
-        print("Number on the left: ", list1[i - 1], 'Number on the right : ', list1[i + 1])
-        break  #выход из цикла,если нашёл число
-else:  #Если не нашёл
+        elif i == len(list1) - 1:  # end
+            print('Number end in list:', list1[i - 1])
+        else:  # Если нашёлся элемент в середине
+            print("Number on the left: ", list1[i - 1], 'Number on the right : ', list1[i + 1])
+            break
+else:
     print('Number is not in the list')
-
-
