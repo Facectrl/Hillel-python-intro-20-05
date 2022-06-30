c1cc189c4f2a15255b212525547f720767927b39
Alexandr Navrotskyi <ledeventsmasters@gmail.com>
2022-06-30 04:26:01 +0300

    11

--- 17.py
+++ 17.py
@@ -12,10 +12,12 @@ import random
 
 cities = ['Киев  ', 'Львов  ', 'Одесса ', 'Днепропетровск',
           'Харьков ', 'Донецк ', 'Запорожье', 'Кривой Рог',
-          'Полтава ', 'Севастополь', 'Симферополь', 'Сумы ',
+          'Полтава ', 'Чернигов ', 'Херсон ', 'Хмельницк'
           'Тернополь', 'Ужгород', 'Херсон ', 'Николаев ',
-          'Хмельницкий', 'Черкассы', 'Чернигов', 'Черновцы'
+          'Хмельницкий', 'Александровка', 'Чернигов', 'Черновцы',
           'Антоновка', 'Белгород', 'Бровары', 'Волгодонск',
+          'Гатчина', 'Дубно', 'Евпатория', 'Житомир','Ровно',
+          'Винница','Кропивницк', 'Луцк', 'Львов', 'Миргород',
           ]
 
 avialable_cities = list(cities)
@@ -24,24 +26,28 @@ first_city = avialable_cities.pop()
 print(first_city)
 
 while True:
-    user = input()
+    user = input('Введите название города: ')
 
     if user.lower()[0] != first_city.lower()[-1]:
-       print('не верно, начинатся должен с буквы', first_city[-1])
+       print('не верно, начинатся должен с буквы', first_city.lower()[-1])
     elif user not in cities:
         print('нет такого города')
     elif user not in avialable_cities:
         print('Город уже выбран')
     else:
+        city_found = False
         avialable_cities.remove(user)  # удаляем город из доступных
         for city in avialable_cities:
             if city.lower()[0] == user.lower()[-1]:
                 user = city
                 avialable_cities.remove(city)
                 print(city)
+                city_found = True
                 break
-
-
+        else:
+            print('Нет городов начинающихся с последней буквы', user[-1])
+            city_found = True
+            break
 
 print('Ваша цепочка:', first_city, user)
 
