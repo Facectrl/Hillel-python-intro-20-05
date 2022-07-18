179f662dedcbe9b7498d935bd9864fb9ff4f3c8a
Alexandr Navrotskyi <ledeventsmasters@gmail.com>
2022-07-18 04:51:58 +0300

    So sad

--- enum_create.py
+++ enum_create.py
@@ -1,55 +1,50 @@
-import enum  # Модуль enum содержит в себе тип для перечисления значений, с возможностью итерирования.
 import abc   # Модуль abc содержит в себе абстрактные классы и методы.
+import enum
 from abc import ABC
 
 
-class Group(enum.Enum):  # Для создания перечисления необходимо создать класc, являющийся наследником класса enum.Enum
-
+class Group_Fop (enum.Enum):
     FOP_GROUP = 1
     FOP_GROUP_2 = 2
     FOP_GROUP_3 = 3
 
-
-# Для создания абстракции необходимо создать класc, являющийся наследником класса abc.ABCMeta
-class Fop(abc.ABC): # Для создания абстракции необходимо создать класc, являющийся наследником класса abc.ABCMeta
+# Для создания абстракции необходимо создать класc, являющийся наследником класса abc.ABC
+class Fop(abc.ABC):  # Для создания абстракции необходимо создать класc, являющийся наследником класса abc.ABCMeta
     def __init__(self, name, turnover, expenses, group):
         self.name = name
-        self.turnover = turnover  # Прибыль предпринимателя
-        self.expenses = expenses  # Расходы предпринимателя
+        self.turnover = turnover  # Прибыль предпринимателя  в квартале
+        self.expenses = expenses  # Расходы предпринимателя в квартале
         self.group = group  # Группа предпринимателя
-
-    def tax_return(self) -> float:
-        return self.turnover - self.expenses
-
-
-    @abc.abstractmethod
-    def tax_calculation(self) -> float:
-        pass
-
-
-    def __str__(self) -> str:
-        return f'{self.name}' + f' - {self.tax_calculation()}'
-
+        self.group = Group_Fop.FOP_GROUP
+        self.group = Group_Fop.FOP_GROUP_2
+        self.group = Group_Fop.FOP_GROUP_3
 
 # single tax до 10% от прожиточного минимума раз на квартал, с 1 января 2022 года - 1430,0 (6500,0 х 22%)
+
+
 class SingleTax(Fop):
     def tax_calculation(self) -> float:
         return self.turnover * 0.22
 
 
-class FopGroup1(Fop, ABC):
+class FopGroup(Fop, ABC):
 
     MAX_INCOME = 1085000    # 167 * 6500 максимальный доход предпринимателя это 167 размеров минимальных зарплат
     SSC = 1430  # single social contribution  - сумма налога на прибыль от предпринимателя
     SINGLE_TAX = 248.10
-     # Минимальная зарплата предпринимателя
+    MINIMAL_SALARY = 6500
 
     def __init__(self, name, turnover, expenses, group):
-        super().__init__(name, turnover, expenses, group)    # Вызов конструктора класса Fop
+        super().__init__(name, turnover, expenses, group)
+        self.group = Group_Fop.FOP_GROUP
+        self.group = Group_Fop.FOP_GROUP_2
+        self.group = Group_Fop.FOP_GROUP_3
 
     def tax_calculation(self) -> float:
         if self.turnover > self.MAX_INCOME:  # Если прибыль предпринимателя больше максимального дохода
-            return self.SSC,(self.MINIMAL_SALARY * 15) * 0.22  # (6500,0 х 15) х 22% = 21450 налог на прибыль
+            return self.SSC, (self.MINIMAL_SALARY * 15) * 0.22  # (6500,0 х 15) х 22% = 21450 налог на прибыль
+        else:
+            Group_Fop.FOP_GROUP = Group_Fop.FOP_GROUP_2   # Переход на вторую группу предпринимателей
 
 
 class FopGroup2(Fop, ABC):
@@ -59,21 +54,67 @@ class FopGroup2(Fop, ABC):
     SINGLE_TAX = 1300.0
     MINIMAL_SALARY = 6500
 
-    def __init__(self, name, turnover, expenses, group):
-        super().__init__(name, turnover, expenses, group)   # Вызов конструктора класса Fop
+    def __init__(self, name, turnover, group):
+        super().__init__(name, turnover, group)
+
+    def tax_calculation(self) -> float:
+        if self.turnover > self.MAX_INCOME:
+            return self.SSC, (self.MINIMAL_SALARY * 15) * 0.22
+        else:
+            Group_Fop.FOP_GROUP2 = Group_Fop.FOP_GROUP_3  # Переход на третью группу предпринимателей
 
 
+class FopGroup3(Fop, ABC): # Класс FopGroup3 наследует класс Fop
 
-
-
-
-class FopGroup3(Fop):
-
-    MAX_INCOME = 7585500  # (1167 минималок)
+    MAX_INCOME = 7585500  # (1167 минималок) Максимальный доход предпринимателя
     SSC = 1430  # single social contribution  - сумма налога на прибыль от предпринимателя
     SINGLE_TAX = 1300.0
     MINIMAL_SALARY = 6500
 
+    def __init__(self, name: object, turnover: object, expenses: object, group: object) -> object:
+        super().__init__(name, turnover, expenses, group)
+
     def tax_calculation(self) -> float:
-        if self.group == Group.FOP_GROUP:
-            return self.turnover * 0.05
+        if self.turnover > self.MAX_INCOME:
+            return self.SSC, (self.MINIMAL_SALARY * 15) * 0.22
+        pass
+
+
+# В конце надо создать N ФОПов подать отчетность за них,
+FOP_Tiger = Fop("Tiger", 75810, 2334, Group_Fop.FOP_GROUP)
+FOP_Sandman = Fop("Sandman", 33310, 25554, Group_Fop.FOP_GROUP_3)
+FOP_Lion = Fop("Lion", 54310, 3454, Group_Fop.FOP_GROUP)
+FOP_Tiger2 = Fop("Tiger", 75810, 2334, Group_Fop.FOP_GROUP_2)
+
+#  и передать их всех на подсчет налогов и получить отчет
+
+class TaxInspector(ABC):
+    def __init__(self, fop):
+        self.fop = fop
+
+    def tax_calculation(self, fop):
+        if isinstance(fop, SingleTax):
+            return fop.tax_calculation()
+        elif isinstance(fop, FopGroup):
+            return fop.tax_calculation()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
